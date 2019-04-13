01982e0555619346a5eef64f7927eb0e9f3098b9
Daltro-Oliveira-Vinuto <daltroov777@gmail.com>
2019-04-13 12:38:22 -0300

    problem 1003

---
+++ 1003.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	long long int a, b, soma;
+
+	scanf("%lld %lld",&a,&b);
+
+	soma = a+b;
+
+	printf("SOMA = %lld\n",soma);
+
+
+	return 0;
+}
